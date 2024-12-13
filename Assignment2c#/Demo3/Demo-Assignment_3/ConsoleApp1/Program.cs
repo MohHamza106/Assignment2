1f0621dedd8f4444bad2c5af76a5805a282ddcde
MohHamza106 <mohamed.ali.hamza349@gmail.com>
2024-12-13 15:23:23 +0200

    Add files via upload

---
+++ Assignment2c#/Demo3/Demo-Assignment_3/ConsoleApp1/Program.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Remoting;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region value types 
+
+            #region implicit casting - safe casting 
+            //int x = 4; // 4 byte
+            //long y =/*(long)*/ x;
+            #endregion
+
+            #region Explicit Casting - unsafe Casting
+            //long x = 101010101010101010; //8byte
+            //int y = x;  /*Error*/
+            // // 
+            // checked
+            //{
+            //    int y = (int)x; /*the command will give you exception so that is wrong */
+            //}
+            //Console.WriteLine(y);
+            //int x = 88;
+            //double y = /*(double)*/x;  /*implicit*/
+            // // 
+
+            //double x = 88.878;
+            //int y = (int) x;          /*Explicit unsaved one */
+            #endregion
+
+            #region Parse [convert from string to any datatype]
+
+            //int Number = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter user ");
+
+            //Console.Write("Enter Name : ");
+            //string Name = Console.ReadLine();
+
+            //Console.Write("Enter your Age ");
+            //int Age = int.Parse(Console.ReadLine());
+
+            //Console.Write(" Enter Salary ");
+            //double salary = double.Parse(Console.ReadLine());
+
+            //Console.Clear();
+
+            //Console.WriteLine("Name : "+ Name  + " Age "+Age + " Salary "+salary);
+            #endregion
+
+            #region Convert [ convert from any Datatype  to any datatype]
+            //Console.WriteLine("Enter user ");
+
+            //Console.Write("Enter Name : ");
+            //string Name = Console.ReadLine();
+
+            //Console.Write("Enter your Age ");
+            //int Age = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write(" Enter Salary ");
+            //double salary = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Clear();
+
+            //Console.WriteLine("Name : " + Name + " Age " + Age + " Salary " + salary);
+            #endregion
+
+
+            #endregion
+
+            #region Operators 
+            #region unary [++,--]
+            //int x = 5;
+            //// x++ , x-- => prefix
+            //// ++x , --x => postfix 
+            //Console.WriteLine(x++);
+            //Console.WriteLine(++x);
+            //Console.WriteLine(x--);
+            //Console.WriteLine(--x);
+
+            #endregion
+
+            #region Binary 
+
+            //int Sum, Mul, Sub, Div, Mod;
+
+            //int a = 10; 
+            //int b = 5;
+
+            //Sum = a+b;
+            //Mul = a*b;
+            //Sub = a-b;
+            //Div = a/b;
+            //Mod = a%b;
+
+            //Console.WriteLine(Sum);
+            //Console.WriteLine(Mul); 
+            //Console.WriteLine(Sub);
+            //Console.WriteLine(Div);
+            //Console.WriteLine(Mod);
+
+
+            #endregion
+
+            #region Assignment 
+            //int x = 10;
+            //x += 10;
+            //x -= 10;
+            //x %= 10;
+            //x *= 10;
+            //x /= 10;
+
+            #endregion
+
+            #region Relational 
+            //int A = 0;
+            //int B = 10;
+
+            //Console.WriteLine(A == B);
+            //Console.WriteLine(A != B);
+            //Console.WriteLine(A <= B);
+            //Console.WriteLine(A >= B);
+            //Console.WriteLine(A < B);
+            //Console.WriteLine(A > B);
+
+            #endregion
+
+            #region logical 
+            //Console.WriteLine(true && false); [Short Circuit]
+            ////  true && true = true 
+            //    true && false = false
+            //    false && true = false
+            //    false && false = false
+            /////////////////////////
+            ////  true || true = true 
+            //    true || false = false
+            //    false || true = false
+            //    false || false = false
+            /////////////////////////////////////////////////////////
+            //Console.WriteLine(true && false); [long Circuit]
+            ////  true & true = true 
+            //    true & false = false
+            //    false & true = false
+            //    false & false = false
+            /////////////////////////
+            ////  true | true = true 
+            //    true | false = false
+            //    false | true = false
+            //    false | false = false
+
+            #endregion
+
+            #region Ternary 
+            //int x = 4;
+
+            //string message = x > 4 ? " X greater than4 " : " X less than 4";
+            #endregion
+
+            #region Operator periorety 
+
+            //int a = 5 ; 
+            //int b = 6; 
+            //int c = 7; 
+            //int d = 8;
+
+            //int result;
+
+            //result = (a + b) * c / d; /* between bracet will be accomplish first then * then / */
+
+            #endregion
+
+            #region String Formating 
+            //    int x = 0;
+            //    int y = 10;
+            //    int result = x+y;
+            //Console.WriteLine("Equation :"+x + " + " + y +" = " + result);
+            ///* it works but not efficient */
+            ///
+
+            /*to be fixed there are two ways */
+
+            #region Composite Formate
+            //int x = 0;
+            //int y = 10;
+            //int result = x + y;
+            //string message = string.Format("Equation  = {0} + {1} = {2}",x,y,result);
+            //Console.WriteLine(message);
+            //Console.WriteLine("Equation  = {0} + {1} = {2}", x, y, result);
+
+
+            #endregion
+
+            #region String Manipolition
+            //int x = 0;
+            //int y = 10;
+            //int result = x + y;
+
+            //Console.WriteLine($"Equation = {x} + {y} = {result}");
+
+            #endregion
+
+
+            #endregion
+
+            #region IF - Else - Switch Case 
+            /*if-statment*/
+
+            //Console.WriteLine("Enter Month Number");
+
+            //int MonthNum = int.Parse(Console.ReadLine());
+
+            //if (MonthNum==1)
+            //{
+            //    Console.WriteLine("Month is Jan");
+            //}
+            //else if (MonthNum==2) 
+            //{
+            //    Console.WriteLine("Month is Feb");
+            //}
+            //else if (MonthNum == 3)
+            //{
+            //    Console.WriteLine("Month is Mar");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not valid "); 
+            //}
+
+
+            /*Switch Case*/
+            //Console.WriteLine("Enter Month Number");
+
+            //int MonthNum = int.Parse(Console.ReadLine());
+
+            //switch (MonthNum )
+            //{
+            //    case 1:
+            //        Console.WriteLine("Month is Jan");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Month is Feb");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Month is Mar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("not valid ");
+            //        break;
+            //}
+
+            #endregion
+
+            #region goto [in Switch case]
+            //int option = int.Parse(Console.ReadLine())
+
+            //switch (option)
+            //{
+            //    case 3000:
+            //        Console.WriteLine("option 03");
+            //        goto case 2000;
+
+            //    case 2000:
+            //        Console.WriteLine("option 02");
+            //        goto case 1000;
+            //    case 1000:
+            //        Console.WriteLine("option 01");
+            //        break;
+            //    default:
+            //        Console.WriteLine("not valid ");
+            //        break;
+            //}
+            #endregion
+
+            #region Evaluation in C# 
+            /*in C# 6 */
+
+            //int Age = int.Parse(Console.ReadLine());
+
+            //switch (Age)
+            //{
+            //    case > 22 :
+            //        Console.WriteLine("greater than 22");
+            //        break;
+
+            //    case <22:
+            //        Console.WriteLine("less than 22");
+            //        break; 
+
+            //    default:
+            //        Console.WriteLine(" Equal 22 ");
+            //        break;
+            //}
+
+            /**********************************************/
+
+            /*C# 7 */
+            //object obj = new object();
+            //obj = "ali";
+            //obj = 18;
+            //obj = true;
+
+
+
+            //switch (obj)
+            //{
+            //    case int Num when Num > 30 && Num <15 : 
+            //        Console.WriteLine($"{Num} is int ");
+            //        break;
+
+            //    case string Name:
+            //        Console.WriteLine($"{Name} is Name ");
+            //        break; 
+
+            //    case bool Flag:
+            //        Console.WriteLine($"{Flag} is bool ");
+            //        break;
+
+            //}
+
+            /*************************************************/
+
+            /*c# 08*/
+            //Console.WriteLine("");
+            //int option = int.Parse(Console.ReadLine());
+
+            //string message = option switch
+            //{
+            //    1 => "option 01",
+            //    2 => "option 02",
+            //    3 => "option 03",
+            //    _ => "Invalid"
+            //}; 
+
+            //Class1 Eo1 = new Class1();
+            //Eo1.Name = "ali";
+            //Eo1.Id = 5050;
+            //Eo1.Age = 22;
+            //string message = Eo1 switch
+            //{
+            //    { Id: 5050, Name: "aliaa", Age: 22 } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}",
+            //    { Id: 5050, Name: "ahmed", Age: 22 } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}",
+            //    { Id: 5050, Name: "mostafa", Age: 22 } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}"
+
+            //};
+            //Console.WriteLine(message);
+
+
+            /*C# 9 */
+
+            //Class1 Eo1 = new Class1();
+            //Eo1.Name = "ali";
+            //Eo1.Id = 5050;
+            //Eo1.Age = 22;
+            //string message = Eo1 switch
+            //{
+            //    {  Age: 22 and 25 } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}",
+            //    {  Age: 22 or 30  } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}",
+            //    {  Age: 22  } => $"{Eo1.Id} : {Eo1.Name} : {Eo1.Age}"
+
+            //};
+            //Console.WriteLine(message);
+
+
+
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
---
+++ Assignment2c#/assigment2/Program.cs
@@ -0,0 +1,306 @@
+﻿using System.Drawing;
+
+namespace assigment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            //Console.Write("this is x ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("this is y ");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+            //Console.WriteLine($"{x}*{y}={x * y}");
+            //Console.WriteLine($"{x}-{y}={x - y}");
+            #endregion
+            #region 1
+            //  Console.Write("enter number=   ");
+            //int number= Convert.ToInt32(Console.ReadLine());
+            //  if (number %3==0 &&number%4==0)   
+            //  {
+            //      Console.WriteLine("yes");
+            //  }
+            //  else
+            //  {
+            //      Console.WriteLine("No");
+            //  }
+            #endregion
+            #region 2
+            //Console.Write("enter number =   ");
+
+            //int number= Convert.ToInt32(Console.ReadLine());
+            //if (number < 0)
+            //{
+            //    Console.WriteLine("negative");
+            //}
+            //else {
+            //    Console.WriteLine("positive");
+
+            //}
+
+
+            #endregion
+            #region 3
+            //Console.Write("this is x ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("this is y ");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("this is z ");
+            //int z = int.Parse(Console.ReadLine());
+            //int max=Math.Max(x,Math.Max(y,z));
+            //int min = Math.Min(x, Math.Min(y, z));
+            //Console.WriteLine($"  max element = {max}");
+            //Console.WriteLine($"  min element = {min}");
+
+            //in other way--------------------
+            //Console.Write("this is x=  ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("this is y=  ");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("this is z=  ");
+            //int z = int.Parse(Console.ReadLine());
+            //if (x > y && x > z)
+            //{
+            //    Console.WriteLine("max is x");
+            //    if (y > z)
+            //    {
+            //        Console.WriteLine("min is z");
+            //    }
+            //    else
+            //        Console.WriteLine("min is y");
+            //}
+            //else if (y > z && y > x)
+            //{
+            //    Console.WriteLine("max is y");
+            //    if (z > x)
+            //    {
+            //        Console.WriteLine("min is x");
+            //    }
+            //    else
+            //        Console.WriteLine("min is z");
+            //}
+            //else
+            //{
+            //    if (z > y && z > x)
+            //    {
+            //        Console.WriteLine("max is z");
+            //        if (y > x)
+            //        {
+            //            Console.WriteLine("min is x");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("min is z");
+            //        }
+            //    }
+            //}
+
+            #endregion
+            #region 4
+            //Console.Write("this is x =  ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine(" number is  even");
+            //}
+            //else if ( x==0)
+            //{
+            //    Console.WriteLine(" the number not even or odd ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" number is  odd");
+            //}
+
+            #endregion
+            #region 5
+            // Console.Write("this is character: ");
+            //char x= Convert.ToChar (Console.ReadLine());
+            // // or   char x = char.Parse(Console.ReadLine());
+            // List<char> list = new List<char> { 'a', 'e', 'I', 'o', 'u' };
+            //     if (x ==list[0] || x == list[1] || x == list[2] || x == list[3] || x == list[4])
+            // {
+            //     Console.WriteLine(" vowel");
+            // }
+            // else 
+            // {
+            //     Console.WriteLine(" Consonant");
+            // }
+            #endregion
+            #region 7
+            //Console.Write("enter the number:   ");
+            //int x =int.Parse (Console.ReadLine());
+            //for (int i = 1; i <= 12; i++) 
+            //{
+            //    Console.WriteLine($"{x}*{i}={x*i}");
+            //}
+            #endregion
+            #region 9
+            //Console.Write("this is x=  ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("this is y=  ");
+            //int y = int.Parse(Console.ReadLine());
+            //double power = Math.Pow(x,y);
+            //Console.WriteLine($"{ power}");
+
+            #endregion
+            #region 10
+            //Console.Write("this is subject1=  ");
+            //int subject1 = int.Parse(Console.ReadLine());
+            //Console.Write("this is subject2=  ");
+            //int subject2 = int.Parse(Console.ReadLine());
+            //Console.Write("this is subject3=  ");
+            //int subject3 = int.Parse(Console.ReadLine());
+            //Console.Write("this is subject4=  ");
+            //int subject4 = int.Parse(Console.ReadLine());
+            //Console.Write("this is subject5 =  ");
+            //int subject5 = int.Parse(Console.ReadLine());
+            //double total =  subject1 + subject2 + subject3 + subject4 + subject5;
+            //double aver =total / 5;
+            //double perstenage = total / 500 * 100;
+            //Console.WriteLine (@$" Total marks:  {total}
+            // Average Marks : {aver}
+            // Percentage: {perstenage} ");
+            #endregion
+            #region 11
+            //Console.Write("enter  number mothn : ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if (number >= 1 && number < 12)
+            //{
+            //    if (number == 1 || number == 3 || number == 5 || number == 7 || number == 8 || number == 10 || number == 12)
+
+            //    {
+            //        Console.WriteLine(" Days in Month: 31");
+            //    }
+            //    else if (number == 4 || number == 6 || number == 9 || number == 11)
+            //    {
+            //        Console.WriteLine(" Days in Month: 30");
+            //    }
+            //    else if (number == 2)
+            //    {
+            //        Console.Write("enter year:  ");  
+            //        int year=int.Parse(Console.ReadLine());
+            //        if ( year%4==0) 
+            //        {
+            //            Console.WriteLine(" Days in Month: 29");
+            //        }
+            //        else
+            //        Console.WriteLine(" Days in Month: 28");
+            //    }
+            //}
+            //else
+            //    Console.WriteLine("this is not month");
+
+
+
+            #endregion
+            #region 17
+            //Console.Write("enter  point1 x1  : ");
+            //int x1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter  point1 y1  : ");
+            //int y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter  point1 x2  : ");
+            //int x2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter  point1 y2  : ");
+            //int y2 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter  point1 x3  : ");
+            //int x3 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("enter  point1 y3  : ");
+            //int y3 = Convert.ToInt32(Console.ReadLine());
+            //int dx1=x2 - x1;
+            //int dy1=y2 - y1;
+            //int dictant = dx1 / dy1;
+            //int dx2 = x3 - x2;
+            //int dy2 = y3 - y2;
+            //int dictant2 = dx2 / dy2 ;
+            //int dx3 = x1 - x3;
+            //int dy3 = y1 - y3;
+            //int dictant3 = (dx3 / dy3 );
+            //Point point1 = new Point( x1,y1);
+            //Point point2 = new Point( x2,y2);
+            //Point point3 = new Point(x3,y3);
+            //if (dictant3 == dictant2|| dictant2==dictant ||dictant==dictant3)
+            //{
+            //    Console.WriteLine("   points lie on a single straight line.");
+            //}
+            //else
+            //    Console.WriteLine("not   points lie on a single straight line");
+            #endregion
+            #region 18
+            //Console.Write("enter time worker using:   ");
+            //float time = float .Parse(Console.ReadLine());
+            //if (time >= 2 && time <= 3)
+            //{
+            //    Console.WriteLine("worker highly efficient. ");
+            //}
+            //else if (time >= 3 && time <= 4)
+            //{
+            //    Console.WriteLine("instructed to increase their speed. ");
+            //}
+            //else if (time >= 4 && time <= 5)
+            //{
+            //    Console.WriteLine("they are provided with training to enhance their speed. \r\n ");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("hey are required to leave the company.  ");
+            //}
+
+            #endregion
+            #region 21
+            //int x = 5;
+            //int y = 8;
+            //Console.WriteLine( $" thisis old x:  {x}");
+            //Console.WriteLine($" thisis old y:  {y}");
+            //x = y;
+            //x = x + 2;
+            ////x increaseby2          x=7
+            ////y change values of x   y=7
+            //Console.WriteLine($" thisis new x:  {x}");
+            //Console.WriteLine($" thisis new x:  {x}");
+            #endregion
+            #region 22
+            //Console.Write("this is name1 ");
+            //string name1 = Console.ReadLine();
+            //Console.Write("this is name2 ");
+            //string name2 = Console.ReadLine();
+            //name2 = name1;
+            //Console.WriteLine($" this is name1: {name1}   ");
+            //Console.WriteLine($"this is name2:{name2}");
+            #endregion
+            #region 23
+            ////b
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+            //(!(false))= true (1)
+
+            #endregion
+            #region 24
+            //d
+            //Console.WriteLine(13 / 2 + " " + 13 % 2);
+            //13/2=6.5 but =6 because datatype int  and 13%2=1 because12%2=0 remind 1
+            #endregion
+            #region 25
+            //-- d
+            //int num = 1, z = 5;
+
+
+            //if (!(num <= 0))     //(!(false))= true
+
+
+            //Console.WriteLine(++num + z++ + " " + ++z);  //num=2 +z=5 =print  7 z=6 ""+ z=6+1 print z=7
+
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+
+
+            #endregion
+
+        }
+
+    }
+}
